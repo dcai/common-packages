d5879b9652252b3d1bb578bee3e58d9cc6dab735
Dongsheng Cai <d@tux.im>
2020-10-08 11:13:49 +1100

    feat(express): add dotenv

--- @packages/generator-express-app/generators/app/templates/jest.config.js
+++ @packages/generator-express-app/generators/app/templates/jest.config.js
@@ -131,7 +131,6 @@ module.exports = {
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
 
   // The test environment that will be used for testing
   // testEnvironment: "jest-environment-jsdom",
--- @packages/generator-express-app/generators/app/templates/src/server.js
+++ @packages/generator-express-app/generators/app/templates/src/server.js
@@ -33,9 +33,11 @@ function onError(error) {
     case 'EACCES':
       console.error(`${bind} requires elevated privileges`);
       process.exit(1);
+      return;
     case 'EADDRINUSE':
       console.error(`${bind} is already in use`);
       process.exit(1);
+      return;
     default:
       throw error;
   }
